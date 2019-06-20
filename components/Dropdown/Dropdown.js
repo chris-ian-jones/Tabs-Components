b80d7415554ab8c1fc56ae46fa7a571a617299bb
Chris Jones <chrisjones@Chriss-MacBook-Pro.local>
2019-06-20 20:07:17 +0100

    WIP: completed up to toggleContent method

--- components/Dropdown/Dropdown.js
+++ components/Dropdown/Dropdown.js
@@ -5,21 +5,19 @@ class Dropdown {
     this.element = element;
     console.log(this.element)    
     // Get the element with the ".dropdown-button" class found in the dropdown element (look at the HTML for context)
-    this.button = this.element.querySelector();
-    
+    this.button = this.element.querySelector('div.dropdown-button');
+    console.log(this.button)
     // assign the reference to the ".dropdown-content" class found in the dropdown element
-    this.content;
-    
+    this.content = this.element.querySelector('div.dropdown-content');
+    console.log(this.content)
     // Add a click handler to the button reference and call the toggleContent method.
-    this.button.addEventListener('click', () => {
-
-    })
+    this.button.addEventListener('click', () => this.toggleContent())
   }
 
   toggleContent() {
-    
+    console.log('toggle method')
     // Toggle the ".dropdown-hidden" class off and on
-    this.content;
+    // this.content.classList.toggle('.dropdown-hidden') ;
   }
 }
 
